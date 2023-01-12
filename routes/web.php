85f1dc1d7ef38691ce23042aca25db3c2ec8c5bb
Arthanz <nathanbara9@gmail.com>
2023-01-12 14:09:52 +0700

    Added Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\GroupController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\group;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +14,13 @@ use App\Models\group;
 |
 */
 
-Route::get('/home', function () {
-    $groups = group::all();
-    return view('home', compact('groups'));
+Route::get('/', function () {
+    return view('welcome');
 });
 
-
-// Route::get('/create-group', function() {
-//     return view ('registerr');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/create-group', [GroupController::class, 'create']);
 Route::post('/store-group', [GroupController::class, 'store']);
@@ -34,3 +30,11 @@ Route::get('/register-leader', 'App\Http\Controllers\LeaderController@createLead
 Route::post('/store-leader', 'App\Http\Controllers\LeaderController@storeLeader');
 Route::get('/register-member', 'App\Http\Controllers\MemberController@createMember');
 Route::post('/store-member', 'App\Http\Controllers\MemberController@storeMember');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
